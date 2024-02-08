a29a9feb75305f162f9a7eaec3bb96fffe317952
Maxim <vedernikovmaxim@rambler.ru>
2024-02-08 18:52:15 +0300

    Update templates "registration.html", added "registration.css"

--- lastar_web_pr/lastar_app/views.py
+++ lastar_web_pr/lastar_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 """Базовая вьюшка, чтобы просто посмотреть. Другие было лень писать)"""
 def index(request):
-    return render(request, 'lastar_app/list_categ_data_st.html')
+    return render(request, 'lastar_app/registration.html')
